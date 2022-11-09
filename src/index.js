fdd05d8fabd3eafeaad7bc3106fc6a6d1e7572ea
sanlorenita-18 <sanlorenita-18@hotmail.com>
2022-11-08 20:32:31 -0500

    Proyecto terminado 2da parte

--- src/index.js
+++ src/index.js
@@ -3,17 +3,14 @@ import validator from './validator.js';
 let verificacion = document.getElementById("btnComprobar")  //este boton me acciona la funcion
 
 verificacion.addEventListener("click", () => {
-
     let inputNumero = document.getElementById("claveTarjeta").value;
     let resumValidator = validator.isValid(inputNumero);
-    // let resunMaskify = validator.maskify(inputNumero);
+    let resunMaskify = validator.maskify(inputNumero);
     
     if (resumValidator) {
+        document.getElementById("numeroDeTarjeta").innerHTML = resunMaskify;
         alert("valido");
     } else {
-        console.log("invalido");
         alert("invalido");
     }
 });
-   
-
--- src/validator.js
+++ src/validator.js
@@ -1,50 +1,52 @@
 const validator = {
   isValid: function (creditCardNumber) {
-    console.log("entro a isvalid" + creditCardNumber);
     let arrayDeNumero = Array.from(creditCardNumber)
-    // let arrayDeNumero = creditCardNumber.split("");
-    console.log("este es el array" + arrayDeNumero);
     let numerosInvertidos = arrayDeNumero.reverse();
-    console.log("numero invertido " + numerosInvertidos);
-    for (let index = 1; index < numerosInvertidos.length; index = index + 2) {
-      numerosInvertidos[index] = numerosInvertidos[index] * 2;
-      console.log("digito por dos " + numerosInvertidos[index]);
-      if (numerosInvertidos[index] >= 10) {
-        numerosInvertidos[index] = numerosInvertidos[index] - 9;
-        console.log("suma dos digitos " + numerosInvertidos[index]);
-      }else{
-        numerosInvertidos[index] = numerosInvertidos[index]
-        console.log("digitox2-10 " + numerosInvertidos[index]);
+    let impar = [];
+    let numPar = [];
+
+    for (let i = 0; i < numerosInvertidos.length; i++) {
+      if ((i + 1) % 2 == 0) {                              //capturando las posiciones pares ¿donde estan los impares?
+        let resultadoxDos = numerosInvertidos[i] * 2;        //esos numeros posicion par * 2
+        if (resultadoxDos >= 10) {
+          let resulDosDigitos = resultadoxDos - 9;
+          numPar.push(resulDosDigitos);
+        } else {
+          let segundoResultado = resultadoxDos;
+          numPar.push(segundoResultado);
+        } 
+      } else {
+        impar.push(parseInt(numerosInvertidos[i]));       
       }
     }
-    console.log("numparmulti " + numerosInvertidos);
-    let sumaTotal = numerosInvertidos.reduce((a,b)=>{
-      let primerDigito = parseInt(a);
-      let segundoDigito = parseInt(b);
-      return primerDigito + segundoDigito;
-    })
-    console.log("sumatotal " + sumaTotal);
-   
-      if (sumaTotal % 10 === 0) {
-        return true;
-      } else {
-        return false;
+    let unionVectores = [...numPar, ...impar]              
+    const valorInicial = 0;
+    const suma = unionVectores.reduce(
+      (valorAnterior, valorActual) => (valorAnterior) + (valorActual),
+      valorInicial
+      );
+
+      if (suma % 10 === 0) {
+            return true;
+          } else {
+            return false;
+          }
+    },
+    maskify: function(creditCardNumber){
+      if (creditCardNumber.length < 4) {
+        return creditCardNumber; 
+      } else{
+        let numerosQueSeven = ''
+        for(let i = creditCardNumber.length-4; i < creditCardNumber.length; i++) {
+          numerosQueSeven += creditCardNumber[i];
+        }
+        let numerosQueNoSeven = ''
+        for (let j = 0; j < creditCardNumber.length -4; j++) {
+          numerosQueNoSeven += '#'
+        }
+        return numerosQueNoSeven + numerosQueSeven;
       }
-  }
-  // ,
-  // maskify: function(creditCardNumber){
-    
-  //   let arrayDelValor = Array.from(creditCardNumber);
-  //   let resultadoDelValor = "";
-  //   for(let i = 0; i < arrayDelValor.length; i++){
-  //    if(i < arrayDelValor.length-4){
-  //     resultadoDelValor =  resultadoDelValor + "#"     
-  //     }else{
-  //       resultadoDelValor = resultadoDelValor + arrayDelValor[i] 
-  //     }
-  //     return resultadoDelValor;
-  //   }
-  // }
+    }
 };
 export default validator
 
@@ -52,4 +54,3 @@ export default validator
 // 2343423413213212
 // 5555555555554444
 // 371449635398431
-
