2c4ae4ebaf13dfab85e62bc1d0a5dc87eb9219f4
Sandra-18 <sanlorenita-18@hotmail.com>
2023-04-18 09:34:52 -0500

    limpiar los inputs

--- src/index.js
+++ src/index.js
@@ -1,28 +1,25 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-const verificacion = document.getElementById("btnComprobar")  //este boton me acciona la funcion
+const verificacion = document.getElementById("btnComprobar"); //este boton me acciona la funcion
 const inputNumero = document.getElementById("claveTarjeta");
-const numTarget = document.getElementById("numeroDeTarjeta")
+const numTarget = document.getElementById("numeroDeTarjeta");
 verificacion.addEventListener("click", () => {
-    let resumValidator = validator.isValid(inputNumero);                             
-    if (resumValidator) {
-
-        alert("valido");
-    } else {
-        alert("invalido");
-    }
-});   
-
+  let resumValidator = validator.isValid(inputNumero.value);
+  if (resumValidator) {
+    alert("valido");
+  } else {
+    alert("invalido");
+  }
+  document.getElementById("formularioDos").reset();
+  numTarget.textContent = "#### #### #### ####"
+});
 inputNumero.addEventListener("input", () => {
-    const regex = /^[^a-zA-Z]*$/
-    const resul = validator.maskify(inputNumero.value) 
-    if(!regex.test(resul)){
-
-        numTarget.textContent = ""
-        inputNumero.value = ""
-    }else{
-        numTarget.textContent = resul
-    }
-    
-})
-
+  const regex = /^[^a-zA-Z]*$/;
+  const resul = validator.maskify(inputNumero.value);
+  if (!regex.test(resul)) {
+    numTarget.textContent = "";
+    inputNumero.value = "";
+  } else {
+    numTarget.textContent = resul;
+  }
+});
